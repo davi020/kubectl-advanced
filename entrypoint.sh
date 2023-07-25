79fc7ca828cc7e13714141ca7574bd21dd67b8d4
JohnsonDavidSME <62434129+davi020@users.noreply.github.com>
2023-07-25 18:00:35 +0530

    Commit:1

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+echo "${KUBE_CONFIG_DATA}" | base64 -d > kubeconfig
+#export KUBECONFIG="${PWD}/kubeconfig"
+#chmod 600 "${PWD}/kubeconfig"
+export KUBECONFIG=kubeconfig
+
+echo "running kubectl command(s)"
+response=$(/bin/sh -c "${INPUT_COMMAND}") 
+echo "${response}"
+{
+  echo "response<<EOF";
+  echo "$response";
+  echo "EOF";
+} >> "${GITHUB_OUTPUT}"
