8e6f49d5f79fdf2e63dc955529d5101610e58cfe
JohnsonDavidSME <62434129+davi020@users.noreply.github.com>
2023-07-25 18:47:29 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,7 @@
 
 set -e
 echo "${KUBE_CONFIG_DATA}" | base64 -d > kubeconfig
-#export KUBECONFIG="${PWD}/kubeconfig"
-#chmod 600 "${PWD}/kubeconfig"
 export KUBECONFIG=kubeconfig
-
 echo "running kubectl command(s)"
 response=$(/bin/sh -c "${INPUT_COMMAND}") 
 echo "${response}"
